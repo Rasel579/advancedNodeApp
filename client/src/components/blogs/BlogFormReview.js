498e0314d49cae7d27a609c5f08a34882e4b3b3e
Ruslan <rasel579@gmail.com>
2019-04-09 12:48:37 +0500

    added to react app an image uploading input

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state  = { file: null };
   renderFields() {
     const { formValues } = this.props;
 
@@ -47,11 +48,22 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event){
+    this.setState({ file: event.target.files });
+    console.log(event.target.files);
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+         <h5>Add an image</h5>
+         <input 
+          type="file" 
+          accept="image/*" 
+          onChange={this.onFileChange.bind(this)}
+           />
 
         {this.renderButtons()}
       </form>
