87049ee0e3a5ce706c45674588f1bdaffdd95c7a
Ruslan <rasel579@gmail.com>
2019-04-09 16:27:57 +0500

    fixed action in client side for making a put request to S3

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,7 +5,9 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyIdS3,
-    secretAccessKey: keys.secretAccessKeyS3
+    secretAccessKey: keys.secretAccessKeyS3,
+    signatureVersion: 'v4',
+    region: 'us-east-2'
 });
 
 module.exports = (app) => {
@@ -13,7 +15,7 @@ module.exports = (app) => {
     const key = `${req.user.id}/${uuid()}.jpeg`;  
     s3.getSignedUrl('putObject', {
         Bucket: 'my-blog-bucket-579',
-        ContentType: 'jpeg',
+        ContentType: 'image/jpeg',
         Key: key
     }, (err, url) => res.send({ key, url }));
   });
