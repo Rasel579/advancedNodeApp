d640329b21666ab0076ad71d24393397d0addf6d
Ruslan Shaykh <37484117+Rasel579@users.noreply.github.com>
2019-04-09 18:21:30 +0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,5 @@ module.exports = {
   mongoURI: 'mongodb://rasel:Gangstar1@ds129536.mlab.com:29536/blog_dev',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
-  accessKeyIdS3: 'AKIAUUCN72IFONM7YLU6',
-  secretAccessKeyS3: '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'
+
 };
